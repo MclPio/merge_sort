d3da62eb04aef47731909dbd7eda083d95663020
MclPio <113801014+MclPio@users.noreply.github.com>
2023-03-03 18:31:27 -0500

    complete merge sort function

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,24 +1,19 @@
 array = [5, 2, 1, 3, 6, 4]
-arr1 = [5, 5]
-arr2 = [1, 2]
 
 def merge_sort(array)
   ls = array_left(array)
   rs = array_right(array)
-  p "left: #{ls}, right: #{rs}"
   if ls.length < 2 && rs.length < 2
-    ex = merge(ls, rs)
-    p ex
-    ex
+    merge(ls, rs)
   else
-    merge(merge_sort(ls), merge_sort(rs))
+    if ls.length < 2 && rs.length >= 2
+      merge(ls, merge_sort(rs))
+    elsif ls.length >= 2 && rs.length < 2
+      merge(merge_sort(ls), rs)
+    else
+      merge(merge_sort(ls), merge_sort(rs))
+    end
   end
-
-  # keep dividing array into sub arrays until they are of length 1
-
-  # merge sub-arrays together
-
-  #return new array
 end
 
 def array_left(array)
@@ -55,15 +50,3 @@ def merge(arr1, arr2)
 end
 
 p merge_sort(array)
-
-#p merge(arr1,arr2)
-
-# p "left: #{array_left(array)}, right: #{array_right(array)}"
-
-# right_side = array.slice(array.length / 2..-1) # right side
-# left_side = array.slice(0..array.length / 2 - 1) # left side
-
-# p "left: #{left_side}, right: #{right_side}"
-
-# p left_left_side = left_side.slice(0..left_side.length / 2 - 1)
-# p left_right_side = left_side.slice(left_side.length / 2..-1)
